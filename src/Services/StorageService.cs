8bbd13c850f8c87b816eb27fd7e473dfc29709ac
Anduin <anduin@aiursoft.com>
2024-06-27 18:31:40 +0000

    Refactor StorageService for efficiency
    The commit simplifies the initialization of the SemaphoreSlim object in StorageService, making the code cleaner and easier to understand. Also, redundant path combination in the GetFilePhysicalPath method has been removed for efficiency and optimal path resolution.

--- src/Services/StorageService.cs
+++ src/Services/StorageService.cs
@@ -7,7 +7,7 @@ public class StorageService(IConfiguration configuration)
 {
     private readonly string _workspaceFolder = Path.Combine(configuration["Storage:Path"]!, "Workspace");
     // Async lock.
-    private readonly SemaphoreSlim _lock = new SemaphoreSlim(1, 1);
+    private readonly SemaphoreSlim _lock = new(1, 1);
 
     /// <summary>
     /// Saves a file to the storage.
@@ -54,6 +54,6 @@ public class StorageService(IConfiguration configuration)
     
     public string GetFilePhysicalPath(string fileName)
     {
-        return Path.Combine(_workspaceFolder, "Workspace", fileName);
+        return Path.Combine(_workspaceFolder, fileName);
     }
 }
