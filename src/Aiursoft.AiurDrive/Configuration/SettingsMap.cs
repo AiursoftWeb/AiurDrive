50e9b819b58b59630f76b9749b2cbeb2d93dc2e6
Anduin Xue <anduin@aiursoft.com>
2026-01-19 10:08:47 +0000

    Fix build.

--- src/Aiursoft.AiurDrive/Configuration/SettingsMap.cs
+++ src/Aiursoft.AiurDrive/Configuration/SettingsMap.cs
@@ -2,7 +2,7 @@ using Aiursoft.AiurDrive.Models;
 
 namespace Aiursoft.AiurDrive.Configuration;
 
-public static class SettingsMap
+public class SettingsMap
 {
     public const string AllowUserAdjustNickname = "Allow_User_Adjust_Nickname";
     public const string MaxSiteStorageInGB = "Max_Site_Storage_In_GB";
@@ -13,18 +13,14 @@ public static class SettingsMap
     /// </summary>
     public const string AllowImagePreview = "Allow_Image_Preview";
 
-    public class FakeLocalizer
-    {
-        public string this[string name] => name;
-    }
 
-    private static readonly FakeLocalizer Localizer = new();
 
     public static readonly List<GlobalSettingDefinition> Definitions = new()
     {
         new GlobalSettingDefinition
         {
             Key = AllowUserAdjustNickname,
+            Name = "Allow User Adjust Nickname",
             Description = "Allow users to adjust their nickname in the profile management page.",
             Type = SettingType.Bool,
             DefaultValue = "True"
@@ -32,6 +28,7 @@ public static class SettingsMap
         new GlobalSettingDefinition
         {
             Key = MaxSiteStorageInGB,
+            Name = "Max Site Storage In GB",
             Description = "Max storage size for each site in GB.",
             Type = SettingType.Number,
             DefaultValue = "10"
@@ -39,6 +36,7 @@ public static class SettingsMap
         new GlobalSettingDefinition
         {
             Key = MaxSitesPerPerson,
+            Name = "Max Sites Per Person",
             Description = "Maximum number of sites each user can create.",
             Type = SettingType.Number,
             DefaultValue = "5"
@@ -46,6 +44,7 @@ public static class SettingsMap
         new GlobalSettingDefinition
         {
             Key = AllowImagePreview,
+            Name = "Allow Image Preview",
             Description = "Allow image preview in the file manager. This is only a front-end switch. Image processing is always enabled.",
             Type = SettingType.Bool,
             DefaultValue = "True"
--- src/Aiursoft.AiurDrive/Models/GlobalSettingsViewModels/SettingViewModel.cs
+++ src/Aiursoft.AiurDrive/Models/GlobalSettingsViewModels/SettingViewModel.cs
@@ -3,6 +3,7 @@ namespace Aiursoft.AiurDrive.Models.GlobalSettingsViewModels;
 public class SettingViewModel
 {
     public required string Key { get; set; }
+    public required string Name { get; set; }
     public required string Description { get; set; }
     public required SettingType Type { get; set; }
     public string? Value { get; set; }
