99feb8ee7e252ceb4781acb7902b57a9937d8b9c
Anduin Xue <anduin@aiursoft.com>
2026-02-11 11:11:27 +0000

    Bump to latest template.

--- src/Aiursoft.AiurDrive/Services/ViewModelArgsInjector.cs
+++ src/Aiursoft.AiurDrive/Services/ViewModelArgsInjector.cs
@@ -26,6 +26,7 @@ namespace Aiursoft.AiurDrive.Services;
 public class ViewModelArgsInjector(
     IStringLocalizer<ViewModelArgsInjector> localizer,
     StorageService storageService,
+    GlobalSettingsService globalSettingsService,
     NavigationState<Startup> navigationState,
     IAuthorizationService authorizationService,
     IOptions<AppSettings> appSettings,
@@ -112,13 +113,16 @@ public class ViewModelArgsInjector(
     {
         var preferDarkTheme = context.Request.Cookies[ThemeController.ThemeCookieKey] == true.ToString();
         toInject.PageTitle = localizer[toInject.PageTitle ?? "View"];
-        toInject.AppName = localizer["AiurDrive"];
+        var projectName = globalSettingsService.GetSettingValueAsync("ProjectName").GetAwaiter().GetResult();
+        var bandName = globalSettingsService.GetSettingValueAsync("BrandName").GetAwaiter().GetResult();
+        var brandHomeUrl = globalSettingsService.GetSettingValueAsync("BrandHomeUrl").GetAwaiter().GetResult();
+        toInject.AppName = projectName;
         toInject.Theme = preferDarkTheme ? UiTheme.Dark : UiTheme.Light;
         toInject.SidebarTheme = preferDarkTheme ? UiSidebarTheme.Dark : UiSidebarTheme.Default;
         toInject.Layout = UiLayout.Fluid;
         toInject.FooterMenu = new FooterMenuViewModel
         {
-            AppBrand = new Link { Text = localizer["AiurDrive"], Href = "https://gitlab.aiursoft.com/aiursoft/aiurdrive" },
+            AppBrand = new Link { Text = bandName, Href = brandHomeUrl },
             Links =
             [
                 new Link { Text = localizer["Home"], Href = "/" },
@@ -195,8 +199,8 @@ public class ViewModelArgsInjector(
         {
             SideLogo = new SideLogoViewModel
             {
-                AppName = localizer["Aiursoft AiurDrive"],
-                LogoUrl = "/logo.svg",
+                AppName = projectName,
+                LogoUrl = GetLogoUrl(context).GetAwaiter().GetResult(),
                 Href = "/"
             },
             SideMenu = new SideMenuViewModel
@@ -298,5 +302,16 @@ public class ViewModelArgsInjector(
                 ]
             };
         }
+    }
+
+
+    private async Task<string> GetLogoUrl(HttpContext context)
+    {
+        var logoPath = await globalSettingsService.GetSettingValueAsync("ProjectLogo");
+        if (string.IsNullOrWhiteSpace(logoPath))
+        {
+            return "/logo.svg";
+        }
+        return storageService.RelativePathToInternetUrl(logoPath, context);
     }
 }
